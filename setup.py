33136721937542773f851c0a091314ddff698a65
Tao Ren <32982291+cliffren@users.noreply.github.com>
2025-10-02 16:06:01 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from distutils.core import setup
 import setuptools
 setup(
     name='scSurvival', # 包的名字
-    version='1.2.0',  # 版本号
+    version='1.3.0',  # 版本号
     description='scSurvival is a new, scalable, and interpretable tool for modeling survival outcomes from single-cell cohort data, with cellular-resolution risk profiling.',  
     author='Tao Ren',
     author_email='renta@ohsu.edu', 
-    url='https://github.com/Cliffthinker/scSurvival', 
+    url='https://github.com/cliffren/scSurvival', 
     packages=setuptools.find_packages(exclude=['*bak', 'example', 'pics', 'libs']),  # 包内不需要引⽤的⽂件夹
     # 依赖包
     install_requires=[
